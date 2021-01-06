373387b305468f3bcfaf11e4fd33f9ebbeab93bc
Sierra Seabrease <sseabrease818@gmail.com>
2021-01-06 15:49:56 -0800

    added is_pacman_in_range

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -6,6 +6,8 @@ public class Ghost{
 	Location myLoc;
 	Map myMap;
 
+	
+
 	public Ghost(String name, Location loc, Map map) {
 		this.myLoc = loc;
 		this.myName = name;
@@ -21,7 +23,20 @@ public class Ghost{
 	}
 
 	public boolean is_pacman_in_range() { 
-		return false;
+		//Gets the sets from all possible locations in the range. 
+		HashSet<Type> upright = myMap.getLoc(new Location(myLoc.x+1, myLoc.y+1)); 
+		HashSet<Type> upleft = myMap.getLoc(new Location(myLoc.x+1, myLoc.y-1)); 
+		HashSet<Type> upmid = myMap.getLoc(new Location(myLoc.x+1, myLoc.y)); 
+		HashSet<Type> midright = myMap.getLoc(new Location(myLoc.x, myLoc.y+1));
+		HashSet<Type> midleft = myMap.getLoc(new Location(myLoc.x, myLoc.y-1));
+		HashSet<Type> downleft = myMap.getLoc(new Location(myLoc.x-1, myLoc.y-1)); 
+		HashSet<Type> downmid = myMap.getLoc(new Location(myLoc.x-1, myLoc.y)); 
+		HashSet<Type> downright = myMap.getLoc(new Location(myLoc.x-1, myLoc.y+1)); 
+
+		//If a pacman type is included in any of these sets then there is a pacman in the range of the ghost
+		return upright.contains(Type.PACMAN)||upleft.contains(Type.PACMAN)||upmid.contains(Type.PACMAN)
+		|| midright.contains(Type.PACMAN)|| midleft.contains(Type.PACMAN)||downleft.contains(Type.PACMAN)||downmid.contains(Type.PACMAN)||downright.contains(Type.PACMAN))
+
 	}
 
 	public boolean attack() {
