ac255796adfe89207c17f2eec8580d606ccd301b
Ritik Sharma <ritik123@Ritiks-MacBook-Pro.local>
2021-01-06 17:07:32 -0500

    Added pacman-get_valid_moves

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -15,7 +15,22 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+		ArrayList<Location> temp = new ArrayList<>();
+		//Above
+		temp.add(new Location(myLoc.x-1, myLoc.y+1));
+		temp.add(new Location(myLoc.x, myLoc.y+1));
+		temp.add(new Location(myLoc.x+1, myLoc.y+1));
+
+		//Left or right
+		temp.add(new Location(myLoc.x-1, myLoc.y));
+		temp.add(new Location(myLoc.x+1, myLoc.y));
+
+		//Below
+		temp.add(new Location(myLoc.x-1, myLoc.y-1));
+		temp.add(new Location(myLoc.x, myLoc.y-1));
+		temp.add(new Location(myLoc.x+1, myLoc.y-1));
+
+		return temp;
 	}
 
 	public boolean move() {
