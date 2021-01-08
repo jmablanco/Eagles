fd59380a1bef1dae707b9986a038362bcc755d86
Sierra Seabrease <sseabrease818@gmail.com>
2021-01-08 10:51:24 -0800

    changed to for loop format

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -23,20 +23,24 @@ public class Ghost{
 	}
 
 	public boolean is_pacman_in_range() { 
-		//Gets the sets from all possible locations in the range. 
-		HashSet<Type> upright = myMap.getLoc(new Location(myLoc.x+1, myLoc.y+1)); 
-		HashSet<Type> upleft = myMap.getLoc(new Location(myLoc.x+1, myLoc.y-1)); 
-		HashSet<Type> upmid = myMap.getLoc(new Location(myLoc.x+1, myLoc.y)); 
-		HashSet<Type> midright = myMap.getLoc(new Location(myLoc.x, myLoc.y+1));
-		HashSet<Type> midleft = myMap.getLoc(new Location(myLoc.x, myLoc.y-1));
-		HashSet<Type> downleft = myMap.getLoc(new Location(myLoc.x-1, myLoc.y-1)); 
-		HashSet<Type> downmid = myMap.getLoc(new Location(myLoc.x-1, myLoc.y)); 
-		HashSet<Type> downright = myMap.getLoc(new Location(myLoc.x-1, myLoc.y+1)); 
+		int x = myLoc.x;
+		int y = myLoc.y;
 
-		//If a pacman type is included in any of these sets then there is a pacman in the range of the ghost
-		return upright.contains(Type.PACMAN)||upleft.contains(Type.PACMAN)||upmid.contains(Type.PACMAN)
-		|| midright.contains(Type.PACMAN)|| midleft.contains(Type.PACMAN)||downleft.contains(Type.PACMAN)||downmid.contains(Type.PACMAN)||downright.contains(Type.PACMAN));
+		//go through all coords that surround myLoc
+		for(int i = x-1; i <= x+1; i++) {
+			for(int j = y-1; j <= y+1; j++) {
 
+				//Don't check coordinates that equal current location or less than 0 
+				if ( (i != x || j != y) && i >= 0 && j >= 0) {
+					HashSet<Map.Type> loc = myMap.getLoc(new Location(i,j));
+
+					if (loc.contains(Map.Type.PACMAN)){
+						return true;   
+					}
+				}
+			}
+		}
+		return false;
 	}
 
 	public boolean attack() {
