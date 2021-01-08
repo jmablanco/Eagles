10067e3c5536a97bb24a0361d35f657d5e80ce8d
Sierra Seabrease <sseabrease818@gmail.com>
2021-01-07 19:53:22 -0800

    Added in pacman consume

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -27,6 +27,11 @@ public class PacMan{
 	}
 
 	public JComponent consume() { 
- 		return null;
+ 		HashSet<Type> currLoc = Map.getLoc(myLoc); //Gets the list of what is located at pacmans location
+		//If the location has a cookie then call eat cookie from Map 
+		if(currLoc.contains(Type.COOKIE)){
+			return Map.eatCookie(myName); 
+		}
+		return null;
 	}
 }
