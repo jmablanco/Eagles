76d4d7ac9a21b4a6e614252192c4ff583d04ad34
Sierra Seabrease <sseabrease818@gmail.com>
2021-01-08 07:15:50 -0800

    Fixing Map to be mymap and removing map changes

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -27,10 +27,10 @@ public class PacMan{
 	}
 
 	public JComponent consume() { 
- 		HashSet<Type> currLoc = Map.getLoc(myLoc); //Gets the list of what is located at pacmans location
+ 		HashSet<Type> currLoc = myMap.getLoc(myLoc); //Gets the list of what is located at pacmans location
 		//If the location has a cookie then call eat cookie from Map 
 		if(currLoc.contains(Type.COOKIE)){
-			return Map.eatCookie(myName); 
+			return myMap.eatCookie(myName); 
 		}
 		return null;
 	}
