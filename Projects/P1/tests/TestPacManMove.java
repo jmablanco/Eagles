7ebc1778d750b58240d216890e0896fd13979955
Teddy <trosejr@terpmail.umd.edu>
2021-01-10 22:43:07 -0500

    added tests for pacman move, fixed pacman.java error

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -5,6 +5,26 @@ import java.io.*;
 public class TestPacManMove extends TestCase {
 
 	public void testPacManMove() throws FileNotFoundException{
-		return null;
+		//Creating A Map 
+		//Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+		int x = 2; 
+		int y = 2; 
+		//Creating Players
+		Ghost ghost = frame.addGhost(new Location(x, y), "ghost1", Color.red); //Creates a red ghost named "ghost1" at location x,y
+		PacMan pacman = frame.addPacMan(new Location(x, y)); //Creates PacMan at location x, y
+		//Start The Game
+		frame.startGame();
+		Map map = frame.getMap(); 
+
+		//Checks if PACMAN is at location before the move.
+		assertTrue(map.getLoc(new Location(x,y)).contains(Map.Type.PACMAN)); 
+
+		//Move pacman
+		pacman.move();
+
+		//Checks if PACMAN was removed from location after move.
+		assertFalse(map.getLoc(new Location(x,y)).contains(Map.Type.PACMAN)); 
+
 	}
 }
