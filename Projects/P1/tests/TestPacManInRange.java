9b766130da46ebb1b66bd11076cdb0d3d599cda1
Sierra Seabrease <sseabrease818@gmail.com>
2021-01-11 14:03:44 -0800

    Switched inRange tests to be in expected locations

--- Projects/P1/tests/TestPacManInRange.java
+++ Projects/P1/tests/TestPacManInRange.java
@@ -5,6 +5,20 @@ import java.io.*;
 public class TestPacManInRange extends TestCase {
 
 	public void testPacManInRange() throws FileNotFoundException{
-		return null;
+		NoFrame frame = new NoFrame();
+		Ghost ghost = frame.addGhost(new Location(10, 5), "clyde", Color.orange);
+		PacMan pacman = frame.addPacMan(new Location(11, 5));
+		
+		//check if ghost is in range
+		assertTrue(pacman.is_ghost_in_range());
+		frame.startGame();
+	
+		NoFrame frame2 = new NoFrame();
+		Ghost ghost2 = frame2.addGhost(new Location(13, 7), "blinky", Color.red);
+		PacMan pacman2 = frame2.addPacMan(new Location(11, 5));
+		
+		//check if ghost is in range
+		assertTrue(pacman2.is_ghost_in_range() == false);
+		frame2.startGame();
 	}
 }
